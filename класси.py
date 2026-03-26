070e0a8d745f1554c4ec1e9ee62debb56cb3a252
filfish2007-cav <filfish2007@gmail.com>
2026-03-26 20:42:25 +0200

    commented second task

--- класси.py
+++ класси.py
@@ -18,27 +18,27 @@ from operator import index
 
 # # task 2
 
-class Student:
-
-    def __init__(self, name, age):
-        self.name = name.capitalize()
-        self.age = age
-
-    def display(self):
-        print(f"Name: {self.name}")
-        print(f"Age: {self.age}")
-
-student1 = Student("John", 25)
-student2 = Student("Michael", 35)
-student3 = Student("Bob", 35)
-
-students = [student1, student2, student3]
-
-for stu in students:
-    print(f"Student {students.index(stu)+1}:")
-    print()
-    stu.display()
-    print()
+# class Student:
+#
+#     def __init__(self, name, age):
+#         self.name = name.capitalize()
+#         self.age = age
+#
+#     def display(self):
+#         print(f"Name: {self.name}")
+#         print(f"Age: {self.age}")
+#
+# student1 = Student("John", 25)
+# student2 = Student("Michael", 35)
+# student3 = Student("Bob", 35)
+#
+# students = [student1, student2, student3]
+#
+# for stu in students:
+#     print(f"Student {students.index(stu)+1}:")
+#     print()
+#     stu.display()
+#     print()
 
 # # task 3
 

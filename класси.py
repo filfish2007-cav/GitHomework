c07c23da0aced4581d1426029a20aa9e5ec12eb2
filfish2007-cav <filfish2007@gmail.com>
2026-03-26 20:29:35 +0200

    The rest of the tasks were done

--- класси.py
+++ класси.py
@@ -1,4 +1,5 @@
 # # task 1
+import math
 from operator import index
 
 
@@ -38,3 +39,53 @@ for stu in students:
     print()
     stu.display()
     print()
+
+# # task 3
+
+# class Circle:
+#     def __init__(self, radius):
+#         self.radius = radius
+#     def area(self):
+#         print(f"Radius is {math.pi * self.radius ** 2}")
+#
+# kolo = Circle(5)
+# kolo.area()
+
+# # task 4
+
+# class BankAccount:
+#     def __init__(self, owner, balance):
+#         self.name = owner.capitalize()
+#         self.balance = int(balance)
+#     def deposit(self, amount):
+#         self.balance += amount
+#     def withdraw(self, amount):
+#         self.balance -= amount
+#     def display(self):
+#         print(f"Name: {self.name}")
+#         print(f"Balance: {self.balance}")
+#
+# user1 = BankAccount("John", 100)
+# user1.deposit(50)
+# user1.withdraw(20)
+# user1.display()
+
+# # task 5
+class Car:
+    def __init__(self, brand, year, is_ready = False):
+        self.brand = brand
+        self.year = year
+        self.is_ready = is_ready
+    def start_engine(self):
+        self.is_ready = True
+        print("The car just started")
+    def move(self):
+        if self.is_ready:
+            print(f"{self.brand} made in {self.year} is Moving ")
+        else:
+            print(f"{self.brand} made in {self.year} is not Moving ")
+
+car1 = Car("Bentley", 2021)
+car1.move()
+car1.start_engine()
+car1.move()

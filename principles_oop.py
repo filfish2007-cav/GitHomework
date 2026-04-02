8f67f3dc5e81c971ca99bd6c051f53ea99c681c9
filfish2007-cav <filfish2007@gmail.com>
2026-04-02 20:48:40 +0300

    Task 3

--- principles_oop.py
+++ principles_oop.py
@@ -70,4 +70,77 @@
 # except ValueError:
 #     print("This figure is not available")
 
+# # task 2
+
+# class Manager:
+#     def __init__(self, name: str, base_salary: int):
+#         self.name = name
+#         self.base_salary = base_salary
+#
+#     def get_salary(self):
+#         return self.base_salary
+#
+#     def show_info(self):
+#         print(f"Менеджер: {self.name}, Зарплата: {self.get_salary()}")
+#
+#
+# class Developer:
+#     def __init__(self, name: str, base_salary: int, work_experience: int):
+#         self.name = name
+#         self.base_salary = base_salary
+#         self.work_experience = work_experience
+#
+#     def get_salary(self):
+#         if self.work_experience > 4:
+#             return self.base_salary * 1.2
+#         return self.base_salary
+#
+#     def show_info(self):
+#         print(f"Розробник: {self.name}, Досвід: {self.work_experience}р., Зарплата: {self.get_salary()}")
+#
+#
+# class Intern:
+#     def __init__(self, name: str, base_salary: int):
+#         self.name = name
+#         self.base_salary = base_salary
+#
+#     def get_salary(self):
+#         return self.base_salary * 0.5
+#
+#     def show_info(self):
+#         print(f"Стажер: {self.name}, Зарплата: {self.get_salary()}")
+#
+#
+# def create_worker() -> Manager | Developer | Intern:
+#     employee_type = input("Тип співробітника (manager/developer/intern): ").strip().lower()
+#
+#     name = input("Ім'я: ").strip()
+#     base_salary = int(input("Базова ставка: "))
+#
+#     if employee_type == "manager":
+#         return Manager(name, base_salary)
+#
+#     elif employee_type == "developer":
+#         experience = int(input("Досвід (років): "))
+#         return Developer(name, base_salary, experience)
+#
+#     elif employee_type == "intern":
+#         return Intern(name, base_salary)
+#     else:
+#         raise ValueError("Такого співробітника нема")
+#
+# workers = []
+#
+# try:
+#     for i in range(3):
+#         print(f"\nВвід даних для співробітника №{i+1}:")
+#         new_worker = create_worker()
+#         workers.append(new_worker)
+#
+#     print("\n--- Звіт по зарплатам ---")
+#     for worker in workers:
+#         worker.show_info()
+#
+# except ValueError as e:
+#     print(f"Помилка: {e}")
 
